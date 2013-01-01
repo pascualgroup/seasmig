14b1d9b80516c53beabaaf912d942e0409de8e85
Daniel <Daniel@Daniel-VAIO.gateway.2wire.net>
2013-01-01 10:42:42 -0500

    reduced dof's MigrationPlusBrownianBaseModel ....

--- src/treelikelihood/MigrationPlusBrowninanBaseModel.java
+++ src/treelikelihood/MigrationPlusBrowninanBaseModel.java
@@ -11,41 +11,45 @@ public class MigrationPlusBrowninanBaseModel implements MigrationPlusContinousSt
 	public class BrownianMotionPlusSeasonalityFunction {
 
 		private double alpha;
-		private double amp;
-		private double phase;
+		private double amp_from;
+		private double phase_from;
+		private double amp_to;
+		private double phase_to;
+		
 
-
-		public BrownianMotionPlusSeasonalityFunction(double alpha_, double amp_, double phase_) {
+		public BrownianMotionPlusSeasonalityFunction(double alpha_, double amp_from_, double phase_from_, double amp_to_, double phase_to) {
 			alpha=alpha_;
-			phase=phase_;
-			amp=amp_;
+			phase_from=phase_from_;
+			amp_from=amp_from_;
+			amp_to=amp_to_;
+			amp_from=amp_from_;
 		}
 
 		@Override
 		public String toString() {
-			// TODO: this... add alpha
-			return "Seasonality: "+amp+"*sin(2Pi*t+2Pi*"+phase+") Wiener: N(0,"+alpha+"*t)";			
+			return "Seasonality: "+amp_from+"*sin(2Pi*t+2Pi*"+phase_from+"),"+amp_to+"*sin(2Pi*t+2Pi*"+phase_to+"), Wiener: N(0,"+alpha+"*t)";			
 		}
 		
 		public double apply(double from_time, double to_time, double from_state, double to_state) {		
 			// TODO: go over this...
 			// When removing the seasonality the adjusted state is assumed to be a Wiener process (Brownian Motion)
 			// Wt-Ws ~ N(0, alpha*(t-s)) (for 0<s<t)
-			double xs = from_state - amp*Math.sin(2*Math.PI*from_time+2*Math.PI*phase);
-			double xt = to_state - amp*Math.sin(2*Math.PI*to_time+2*Math.PI*phase);
+			double xs = from_state - amp_from*Math.sin(2*Math.PI*from_time+2*Math.PI*phase_from);
+			double xt = to_state - amp_to*Math.sin(2*Math.PI*to_time+2*Math.PI*phase_to);
 			double var = Math.abs(to_time-from_time)*alpha;			
-			return Math.log(1/Math.sqrt(2*Math.PI*var))+(xt-xs)*(xt-xs)/(2*var); 
+			return -Math.log(2*Math.PI*var)/2.0+(xt-xs)*(xt-xs)/(2*var); 
 		}
 
 	}
 
 	// Constructor	
-	public MigrationPlusBrowninanBaseModel(MigrationBaseModel migrationBaseModel_, double[][] alphas, double[][] amp, double[][] phase) {
+	public MigrationPlusBrowninanBaseModel(MigrationBaseModel migrationBaseModel_, double[][] alphas, double[] amp, double[] phase) {
+		// TODO: Lookup i and j from to....
 		migrationBaseModel=migrationBaseModel_;
 		logSeasonalStatesTransitionProbabilities = new BrownianMotionPlusSeasonalityFunction[alphas.length][alphas.length];		
 		for (int i=0;i<alphas.length;i++) {
 			for (int j=0;j<alphas.length;j++) {
-				logSeasonalStatesTransitionProbabilities[i][j]=new BrownianMotionPlusSeasonalityFunction(alphas[i][j],amp[i][j],phase[i][j]);
+				logSeasonalStatesTransitionProbabilities[i][j]=new BrownianMotionPlusSeasonalityFunction(alphas[i][j],amp[i],phase[i],amp[j],phase[j]);
 			}
 		}
 	}
