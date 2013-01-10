a3db3c834f24872fcb0951cede9423325af8af01
dzinder <dzinder@CSCS-BIOLAB2.adsroot.itcs.umich.edu>
2013-01-10 16:30:59 -0500

    partially working but slow...

--- src/treelikelihood/TaylorMatrixExp.java
+++ src/treelikelihood/TaylorMatrixExp.java
@@ -11,13 +11,13 @@ public class TaylorMatrixExp implements MatrixExponentiator {
 	Vector<Double> cachedScale = new Vector<Double>();
 	DoubleMatrix2D Q;
 	// Precision Parameters...
-	int nTaylor = 500;	
+	int nTaylor = 1000;	
+	double minValue = 1E-90;
 	
 	// Cache Parameters
 	static final int maxCachedTransitionMatrices = 16000;
 	
 	DoubleFactory2D F = DoubleFactory2D.dense; // TODO: is parallelizable? 	
-	
 
 	public TaylorMatrixExp(DoubleMatrix2D Q_) {
 		Q = Q_;
@@ -29,12 +29,28 @@ public class TaylorMatrixExp implements MatrixExponentiator {
 			cachedScale.add(i,scale);
 			next = next.zMult(Q,null,scale/(i+1.0),0,false,false);
 			scale = getScale(next);
-			next.assign(cern.jet.math.tdouble.DoubleFunctions.mult(1.0/scale));
+			next.assign(cern.jet.math.tdouble.DoubleFunctions.div(scale));	
+			if (scale<minValue) {
+				nTaylor = i-1;
+			}
 		}
 	}
 
 	private double getScale(DoubleMatrix2D Q) {
-		double scale = Math.sqrt(Math.abs(Q.getMinLocation()[0])*Math.abs(Q.getMaxLocation()[0])); 
+		// TODO: redo this...
+		double minScale=Double.MAX_VALUE;;
+		double maxScale=0;
+		for (int i=0; i<Q.rows(); i++) {
+			for (int j=0; j<Q.rows(); j++) {
+				if (Math.abs(Q.get(i, j))<minScale) {
+					minScale = Math.abs(Q.get(i, j));
+				}
+				if (Math.abs(Q.get(i, j))>maxScale) {
+					maxScale = Math.abs(Q.get(i, j));
+				}
+			}
+		}
+		double scale = Math.sqrt(minScale*maxScale);
 		if (scale!=0) 
 			return scale;
 		else
@@ -49,8 +65,24 @@ public class TaylorMatrixExp implements MatrixExponentiator {
 			DoubleMatrix2D taylorn = cachedQnDivFactorialN.get(i).copy().assign(cern.jet.math.tdouble.DoubleFunctions.mult(cachedScale.get(i)*Math.pow(t, i)));
 			result.assign(taylorn, cern.jet.math.tdouble.DoubleFunctions.plus);				
 		}
+		
+		for (int i=0;i<result.rows();i++) {
+			for (int j=0;j<result.rows();j++) {
+				if (result.get(i, j)<0) {
+					result.set(i, j, 0);
+				}
+				if (result.get(i,j)>1) {
+					result.set(i, j, 1);
+				}
+				if (result.get(i, j)==Double.NaN) {
+					System.err.println("result.get(i, j)==Double.NaN");
+				}
+				
+			}
+		}
+		//System.err.println("\nQ:"+Q.toString()+"\nt: "+t+"\nexpm:"+result.toString());
 		return result; 	
-
+		
 	}
 	
 
