0711ed83df35d8659403c88537713d8b797877c7
dzinder <dzinder@CSCS-BIOLAB2.adsroot.itcs.umich.edu>
2013-01-11 16:17:47 -0500

    removed scale from matrix exp

--- src/treelikelihood/TaylorMatrixExp.java
+++ src/treelikelihood/TaylorMatrixExp.java
@@ -8,15 +8,14 @@ import cern.colt.matrix.tdouble.DoubleMatrix2D;
 public class TaylorMatrixExp implements MatrixExponentiator {
 
 	// Precision Parameters...	
-	static final double minValue = Double.MIN_VALUE*1024.0;
+	static final double precision = 1E-15;
 	
 	// Cache 
 	static final int maxCachedTransitionMatrices = 16000;		
 	Vector<DoubleMatrix2D> cachedQnDivFactorialN = new Vector<DoubleMatrix2D>();
-	Vector<Double> cachedScale = new Vector<Double>();
 	DoubleMatrix2D Q;
 	DoubleMatrix2D zeroMatrix;
-	private int nTaylor = Integer.MAX_VALUE;
+	private int nTaylor = 100;
 	
 	DoubleFactory2D F = DoubleFactory2D.dense; 
 	
@@ -24,39 +23,11 @@ public class TaylorMatrixExp implements MatrixExponentiator {
 		Q = Q_;
 		zeroMatrix = F.make(Q.rows(),Q.rows(),0);
 		DoubleMatrix2D next = F.identity(Q.rows()); // Q^0/0! = I
-		double scale = getScale(next);		
 		int nTaylor = Integer.MAX_VALUE;		
 		for (int i=0;i<nTaylor;i++) { // Q^N/N! = Q^(N-1)/(N-1)!*Q/N
 			cachedQnDivFactorialN.add(i,next);
-			cachedScale.add(i,scale);
-			next = next.zMult(Q,null,scale/(i+1.0),0,false,false);
-			scale = getScale(next);
-			next.assign(cern.jet.math.tdouble.DoubleFunctions.div(scale));	
-			if (scale<minValue) {
-				nTaylor = i-1;
-			}
+			next = next.zMult(Q,null,1.0/(i+1.0),0,false,false);
 		}
-	}
-
-	private double getScale(DoubleMatrix2D Q) {
-		// TODO: redo this...
-		double minScale=Double.MAX_VALUE;;
-		double maxScale=0;
-		for (int i=0; i<Q.rows(); i++) {
-			for (int j=0; j<Q.rows(); j++) {
-				if (Math.abs(Q.get(i, j))<minScale) {
-					minScale = Math.abs(Q.get(i, j));
-				}
-				if (Math.abs(Q.get(i, j))>maxScale) {
-					maxScale = Math.abs(Q.get(i, j));
-				}
-			}
-		}
-		double scale = Math.sqrt(minScale*maxScale);
-		if (scale!=0) 
-			return scale;
-		else
-			return 1.0;
 	}
 
 	@Override
@@ -71,11 +42,11 @@ public class TaylorMatrixExp implements MatrixExponentiator {
 		DoubleMatrix2D result = zeroMatrix.copy();
 
 		for (int i=nTaylor-1;i>=0;i=i-2) {
-			DoubleMatrix2D taylorn = cachedQnDivFactorialN.get(i).copy().assign(cern.jet.math.tdouble.DoubleFunctions.mult(cachedScale.get(i)*Math.pow(t, i)));
+			DoubleMatrix2D taylorn = cachedQnDivFactorialN.get(i).copy().assign(cern.jet.math.tdouble.DoubleFunctions.mult(/*cachedScale.get(i)**/Math.pow(t, i)));
 			result.assign(taylorn, cern.jet.math.tdouble.DoubleFunctions.plus);
 		}
 		for (int i=nTaylor-2;i>=0;i=i-2) {
-			DoubleMatrix2D taylorn = cachedQnDivFactorialN.get(i).copy().assign(cern.jet.math.tdouble.DoubleFunctions.mult(cachedScale.get(i)*Math.pow(t, i)));
+			DoubleMatrix2D taylorn = cachedQnDivFactorialN.get(i).copy().assign(cern.jet.math.tdouble.DoubleFunctions.mult(/*cachedScale.get(i)**/Math.pow(t, i)));
 			result.assign(taylorn, cern.jet.math.tdouble.DoubleFunctions.plus);
 		}
 		
@@ -87,12 +58,12 @@ public class TaylorMatrixExp implements MatrixExponentiator {
 		for (int i=0;i<result.rows();i++) {
 			for (int j=0;j<result.rows();j++) {
 				if (result.get(i, j)<0) {
-					result.set(i, j, minValue);
+					result.set(i, j, precision);
 					System.err.println("result.get(i, j)<0");
 					System.err.println("\nQ:"+Q.toString()+"\nt: "+t+"\nexpm:"+result.toString());
 				}
 				if (result.get(i,j)>1) {
-					result.set(i, j, 1-minValue);
+					result.set(i, j, 1-precision);
 					System.err.println("result.get(i, j)>1");
 					System.err.println("\nQ:"+Q.toString()+"\nt: "+t+"\nexpm:"+result.toString());
 				}
