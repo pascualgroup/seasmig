4e9c232f4f066de45ab3c949298cb689a2417004
dzinder <dzinder@CSCS-BIOLAB2.adsroot.itcs.umich.edu>
2013-01-14 18:01:27 -0500

    parse for mathematica...

--- parsesamples.py
+++ parsesamples.py
@@ -6,9 +6,11 @@ file = open('samples.jsons')
 jsonsFile = JsonsFile(file)
 # TODO: import header and data ....
 
-result="";
+result="{\"twoMatrixPhase\",\"rates\",\"rates2\"}\n";
 for obj in jsonsFile:
-    result+="{"    
+    result+="{"  
+    result+=str(obj['twoMatrixPhase'])
+    result+=","  
     result+=str(obj['rates']).replace("[","{").replace("]","}")
     result+=","
     result+=str(obj['rates2']).replace("[","{").replace("]","}")
