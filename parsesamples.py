c088b19ff3bbd90e60bdd83c9f93e07d86d0109f
dzinder <dzinder@CSCS-BIOLAB2.adsroot.itcs.umich.edu>
2013-01-14 20:02:35 -0500

    parsesamples...

--- parsesamples.py
+++ parsesamples.py
@@ -4,16 +4,17 @@ from jsons import *
 
 file = open('samples.jsons')
 jsonsFile = JsonsFile(file)
-# TODO: import header and data ....
 
-result="{\"twoMatrixPhase\",\"rates\",\"rates2\"}\n";
+first = jsonsFile.next()
+result="{"
+for key in first.keys():
+    result+=key
+    if key!=first.keys()[-1]:
+        result+=","
+result+="}\n"
+
 for obj in jsonsFile:
-    result+="{"  
-    result+=str(obj['twoMatrixPhase'])
-    result+=","  
-    result+=str(obj['rates']).replace("[","{").replace("]","}")
-    result+=","
-    result+=str(obj['rates2']).replace("[","{").replace("]","}")
-    result+="}\n"
+    result+=str(obj.values()).replace("[","{").replace("]","}")+"\n"    
+
 print result     
 file.close()
