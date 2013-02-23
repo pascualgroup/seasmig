3472ac12c9d052f28c1738ba3e026c50d1cf079e
dzinder <dzinder@CSCS-BIOLAB2.adsroot.itcs.umich.edu>
2013-02-23 16:57:06 -0500

    Ready for cluster test...

---
+++ src/treelikelihood/MigrationPlusContinousStateBaseModel.java
@@ -0,0 +1,10 @@
+package treelikelihood;
+
+public interface MigrationPlusContinousStateBaseModel {
+
+	double logprobability(int from_location, int to_location, double from_time,
+			double to_time, double from_state, double to_state);
+
+	String print();
+
+}
