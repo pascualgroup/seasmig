bbd4962f3f78803a459749ae8d766187cdeb9f48
dzinder <dzinder@CSCS-BIOLAB2.adsroot.itcs.umich.edu>
2013-04-22 17:44:04 -0400

    Decoupling mc3kit...

--- src/mc3kit/ChainParity.java
+++
@@ -1,7 +0,0 @@
-package mc3kit;
-
-public enum ChainParity
-{
-  EVEN,
-  ODD
-}
--- src/mc3kit/partition/IndexAssociator.java
+++
@@ -1,9 +0,0 @@
-package mc3kit.partition;
-
-import java.io.Serializable;
-
-import mc3kit.*;
-
-public interface IndexAssociator extends Serializable {
-  void associate(int itemIndex, int groupIndex) throws MC3KitException;
-}
