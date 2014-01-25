6632072d0d3f0985e3f2e615056ed0ebe0900279
dzinder <dzinder@CSCS-BIOLAB2.adsroot.itcs.umich.edu>
2014-01-25 16:39:02 -0500

    alternative tree output...

--- src/seasmig/util/AltTreeOutput.java
+++ src/seasmig/util/AltTreeOutput.java
@@ -4,26 +4,22 @@ public class AltTreeOutput {
 	private String branches="";
 	private String nodes="";
 	public void addNode(int postOrderIndex, double time, int loc, float layout, String seq) {
-		if (getNodes().length()>0)
-			setNodes(getNodes() + ",");
-		setNodes(getNodes() + "{"+Integer.toString(postOrderIndex)+","+String.format("%.3f", time)+","+Integer.toString(loc)+","+String.format("%.3f", layout)+","+seq+"}");			
+		if (nodes.length()>0)
+			nodes+=",";
+		nodes+="{"+Integer.toString(postOrderIndex)+","+String.format("%.3f", time)+","+Integer.toString(loc)+","+String.format("%.3f", layout)+","+seq+"}";			
 	}
 	public void addBranch(int from, int to) {
-		if (getNodes().length()>0)
-			setNodes(getNodes() + ",");
-		setNodes(getNodes() + "{"+Integer.toString(from)+","+Integer.toString(to)+"}");	
+		if (branches.length()>0)
+			branches+=",";
+		branches+="{"+Integer.toString(from)+","+Integer.toString(to)+"}";	
 		
 	}
 	public String getNodes() {
 		return nodes;
 	}
-	public void setNodes(String nodes) {
-		this.nodes = nodes;
-	}
+
 	public String getBranches() {
 		return branches;
 	}
-	public void setBranches(String branches) {
-		this.branches = branches;
-	}
+	
 }
